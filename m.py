3cdb1b5012b6394272cdb58ebd4ddfdc5c36f948
royalmobile2359 <royalmobile2359@gmail.com>
2024-09-17 22:23:08 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7543594921:AAEb0Qs9usGggKGl0Q-iZ-JimDzJhtv01e0')
+bot = telebot.TeleBot('7496204447:AAHK9pp5_jf7jiX07jEPSLkoQVO4i_MM6k8')
 
 # Admin user IDs
-admin_id = ["5894556386"]
+admin_id = ["7411041233"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
